245f376c1eeabb002a5bed1ce12dfab9340bca70
DCOMP\David <david.pozhar@gmail.com>
2024-11-08 12:47:49 +0100

    created entities API's

---
+++ hometask8/src/api/commentsAPI.ts
@@ -0,0 +1,17 @@
+import { axiosInstance } from ".";
+import { IComment } from "../types/IComment";
+
+export const CommentsAPI = {
+    async comments(exhibitID : number): Promise<IComment[] | any> {
+        const url = `/api/exhibits/${exhibitID}/comments`;
+        return await axiosInstance.get(url);
+    },
+    async createComment(exhibitID : number, text: string) {
+        const url = `/api/exhibits/${exhibitID}/comments`;
+        return await axiosInstance.post(url, {text});
+    },
+    async deleteComment(commentID: number) {
+        const url = `/api/exhibits/exhibitID/comments/${commentID}`;
+        return await axiosInstance.delete(url);
+    }
+}
---
+++ hometask8/src/api/exhibitsAPI.ts
@@ -0,0 +1,27 @@
+import { axiosInstance, axiosUploadInstance } from ".";
+import { IExhibit } from "../types/IExhibit";
+
+const exhibits_BASE_URL = '/api/exhibits';
+
+export const ExhibitsAPI = {
+    async exhibits(page: number, limit?: number): Promise<IExhibit[] | any> {
+        const url = exhibits_BASE_URL;
+        return await axiosInstance.get(url, {
+            params: { page, limit }
+        });
+    },
+    async myExhibits(page: number, limit?: number): Promise<IExhibit[] | any> {
+        const url = `${exhibits_BASE_URL}/my-posts`;
+        return await axiosInstance.get(url, {
+            params: { page }
+        });
+    },
+    async createExhibit(exhibitData: FormData): Promise<IExhibit | any> {
+        const url = exhibits_BASE_URL;
+        return await axiosUploadInstance.post(url, exhibitData);
+    },
+    async deleteExhibit(id: number) {
+        const url = `${exhibits_BASE_URL}/${id}`;
+        return await axiosInstance.delete(url);
+    }
+}
