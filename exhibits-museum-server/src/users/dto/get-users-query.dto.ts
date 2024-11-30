296238bfa8f137e1e5b3fcc7d822339c72893aef
david.pozhar@gmail.com <david.pozhar@gmail.com>
2024-11-30 17:35:37 +0100

    fixed get user by id bug

--- exhibits-museum-server/src/users/dto/get-users-query.dto.ts
+++ exhibits-museum-server/src/users/dto/get-users-query.dto.ts
@@ -1,10 +1,15 @@
-import { IsNumber, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsInt, IsOptional, IsString } from "class-validator";
 
 export class GetUsersQueryDto {
+    @ApiProperty({ example: 1, description: 'Unique identifier for the user' })
+    @Type(() => Number)
     @IsOptional()
-    @IsNumber()
+    @IsInt({ message: 'ID must be an integer' })
     id?: number;
 
+    @ApiProperty({ example: 'john_doe', description: 'Unique username of the user' })
     @IsOptional()
     @IsString({ message: 'Username must be a valid string' })
     username?: string;
--- exhibits-museum-server/src/users/users.controller.ts
+++ exhibits-museum-server/src/users/users.controller.ts
@@ -11,11 +11,11 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get('/')
-    @ApiOperation({ summary: "Get all users" })
+    @ApiOperation({ summary: "Get user by ID or username" })
     @ApiQuery({ name: 'id', type: Number, required: false, description: 'The unique ID of the user' })
     @ApiQuery({ name: 'username', type: String, required: false, description: 'The username of the user' })
     @ApiResponse({ status: 200, type: [User] })
-    @ApiResponse({ status: 400, description: 'Invalid query parameters or failed request.' })
+    @ApiResponse({ status: 400, description: 'Invalid query parameters or failed request' })
     async getUserByQuery(@Query() query: GetUsersQueryDto) {
         const user = await this.usersService.getUser(query);
         return plainToInstance(User, user, { excludeExtraneousValues: true });
